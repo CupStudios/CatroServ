22e1fc434e1e2000acce8c4d18c4314e96e727f7
CupStudios <119907019+CupStudios@users.noreply.github.com>
2024-09-15 16:05:21 -0600

    Delete a directory
    Files weren't necessary anymore.

--- a/v1.0/getrequests.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "utils.php";
-
-if (isset($_GET['ip']) && isset($_GET['type']) && $_GET['ip'] != null && $_GET['type'] != null) {
-	echo getRequests($_GET['ip'], $_GET['type']);
-} else {
-	echo logErr(array("error" => -1)); // IP not provided
-}
-?>
